af0666028582550dd126aa7b00c4c27d2410e882
zzz1eep <y.h0987134175@gmail.com>
2018-05-31 01:37:39 +0800

    app

---
+++ app.py
@@ -0,0 +1,394 @@
+from flask import Flask, request, abort
+import requests
+import re
+from bs4 import BeautifulSoup
+import random
+
+from linebot import (
+	LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+	InvalidSignatureError
+)
+from linebot.models import (
+	MessageEvent, TextMessage, TextSendMessage,
+)
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('JXJh+DghvpLrJCg7DIEy+1c5LmspsNuHcyNkOhpdcwMMEte/9TYjVWL2kMy++Q4GrlsRHvw/sYQhWqcBD9chh3jBv+lJKlREHasNf+c2wxWh4wOwZRwafbTunG4M0BWgVMWFNaz1o9IqKBS/euk+tQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('02a3f2b8daaa79b47d2319e04d4e5d3f')
+
+@app.route("/callback", methods=['POST'])
+def callback():
+	# get X-Line-Signature header value
+	signature = request.headers['X-Line-Signature']
+	
+	# get request body as text
+	body = request.get_data(as_text=True)
+	app.logger.info("Request body: " + body)
+	
+	# handle webhook body
+	try:
+		handler.handle(body, signature)
+	except InvalidSignatureError:
+		abort(400)
+	
+	return 'OK'
+
+
+
+def pattern_mega(text):
+    patterns = [
+        'mega', 'mg', 'mu', 'ＭＥＧＡ', 'ＭＥ', 'ＭＵ',
+        'ｍｅ', 'ｍｕ', 'ｍｅｇａ', 'GD', 'MG', 'google',
+    ]
+    for pattern in patterns:
+        if re.search(pattern, text, re.IGNORECASE):
+            return True
+
+def pattern_av(text):
+    patterns = [
+    '日本',        
+    ]
+    for pattern in patterns:
+        if re.search(pattern, text, re.IGNORECASE):
+            return True
+def av():
+    target_url = 'https://www.jkforum.net/forum.php?mod=forumdisplay&action=list&fid=1405'
+    print('Start parsing AV....')
+    print('Start parsing AV....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_av(titleURL.text):
+            title = titleURL.text
+            if '796770' in titleURL['href']:
+                continue
+            link = 'http://www.jkforum.net/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
+
+def eyny_movie():
+    target_url = 'http://www.eyny.com/forum-205-1.html'
+    print('Start parsing eynyMovie....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ''
+    for titleURL in soup.select('.bm_c tbody .xst'):
+        if pattern_mega(titleURL.text):
+            title = titleURL.text
+            if '11379780-1-3' in titleURL['href']:
+                continue
+            link = 'http://www.eyny.com/' + titleURL['href']
+            data = '{}\n{}\n\n'.format(title, link)
+            content += data
+    return content
+
+def apple_news():
+    target_url = 'https://tw.appledaily.com/new/realtime'
+    print('Start parsing appleNews....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('.rtddt a'), 0):
+        if index == 5:
+            return content
+        link = data['href']
+        content += '{}\n\n'.format(link)
+    return content
+
+def get_page_number(content):
+    start_index = content.find('index')
+    end_index = content.find('.html')
+    page_number = content[start_index + 5: end_index]
+    return int(page_number) + 1
+
+
+def craw_page(res, push_rate):
+    soup_ = BeautifulSoup(res.text, 'html.parser')
+    article_seq = []
+    for r_ent in soup_.find_all(class_="r-ent"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                rate = r_ent.find(class_="nrec").text
+                url = 'https://www.ptt.cc' + link
+                if rate:
+                    rate = 100 if rate.startswith('爆') else rate
+                    rate = -1 * int(rate[1]) if rate.startswith('X') else rate
+                else:
+                    rate = 0
+                # 比對推文數
+                if int(rate) >= push_rate:
+                    article_seq.append({
+                        'title': title,
+                        'url': url,
+                        'rate': rate,
+                    })
+        except Exception as e:
+            # print('crawPage function error:',r_ent.find(class_="title").text.strip())
+            print('本文已被刪除', e)
+    return article_seq
+
+
+def crawl_page_gossiping(res):
+    soup = BeautifulSoup(res.text, 'html.parser')
+    article_gossiping_seq = []
+    for r_ent in soup.find_all(class_="r-ent"):
+        try:
+            # 先得到每篇文章的篇url
+            link = r_ent.find('a')['href']
+
+            if link:
+                # 確定得到url再去抓 標題 以及 推文數
+                title = r_ent.find(class_="title").text.strip()
+                url_link = 'https://www.ptt.cc' + link
+                article_gossiping_seq.append({
+                    'url_link': url_link,
+                    'title': title
+                })
+
+        except Exception as e:
+            # print u'crawPage function error:',r_ent.find(class_="title").text.strip()
+            # print('本文已被刪除')
+            print('delete', e)
+    return article_gossiping_seq
+
+
+def ptt_gossiping():
+    rs = requests.session()
+    load = {
+        'from': '/bbs/Gossiping/index.html',
+        'yes': 'yes'
+    }
+    res = rs.post('https://www.ptt.cc/ask/over18', verify=False, data=load)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    all_page_url = soup.select('.btn.wide')[1]['href']
+    start_page = get_page_number(all_page_url)
+    index_list = []
+    article_gossiping = []
+    for page in range(start_page, start_page - 2, -1):
+        page_url = 'https://www.ptt.cc/bbs/Gossiping/index{}.html'.format(page)
+        index_list.append(page_url)
+
+    # 抓取 文章標題 網址 推文數
+    while index_list:
+        index = index_list.pop(0)
+        res = rs.get(index, verify=False)
+        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+        if res.status_code != 200:
+            index_list.append(index)
+            # print u'error_URL:',index
+            # time.sleep(1)
+        else:
+            article_gossiping = crawl_page_gossiping(res)
+            # print u'OK_URL:', index
+            # time.sleep(0.05)
+    content = ''
+    for index, article in enumerate(article_gossiping, 0):
+        if index == 15:
+            return content
+        data = '{}\n{}\n\n'.format(article.get('title', None), article.get('url_link', None))
+        content += data
+    return content
+
+
+def ptt_beauty():
+    rs = requests.session()
+    res = rs.get('https://www.ptt.cc/bbs/Beauty/index.html', verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    all_page_url = soup.select('.btn.wide')[1]['href']
+    start_page = get_page_number(all_page_url)
+    page_term = 2  # crawler count
+    push_rate = 10  # 推文
+    index_list = []
+    article_list = []
+    for page in range(start_page, start_page - page_term, -1):
+        page_url = 'https://www.ptt.cc/bbs/Beauty/index{}.html'.format(page)
+        index_list.append(page_url)
+
+    # 抓取 文章標題 網址 推文數
+    while index_list:
+        index = index_list.pop(0)
+        res = rs.get(index, verify=False)
+        # 如網頁忙線中,則先將網頁加入 index_list 並休息1秒後再連接
+        if res.status_code != 200:
+            index_list.append(index)
+            # print u'error_URL:',index
+            # time.sleep(1)
+        else:
+            article_list = craw_page(res, push_rate)
+            # print u'OK_URL:', index
+            # time.sleep(0.05)
+    content = ''
+    for article in article_list:
+        data = '[{} push] {}\n{}\n\n'.format(article.get('rate', None), article.get('title', None),
+                                             article.get('url', None))
+        content += data
+    return content
+
+
+def ptt_hot():
+    target_url = 'http://disp.cc/b/PttHot'
+    print('Start parsing pttHot....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for data in soup.select('#list div.row2 div span.listTitle'):
+        title = data.text
+        link = "http://disp.cc/b/" + data.find('a')['href']
+        if data.find('a')['href'] == "796-59l9":
+            break
+        content += '{}\n{}\n\n'.format(title, link)
+    return content
+
+def movie():
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('ul.filmNextListAll a')):
+        if index == 20:
+            return content
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
+
+
+def technews():
+    target_url = 'https://technews.tw/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+
+    for index, data in enumerate(soup.select('article div h1.entry-title a')):
+        if index == 12:
+            return content
+        title = data.text
+        link = data['href']
+        content += '{}\n{}\n\n'.format(title, link)
+    return content
+
+
+def panx():
+    target_url = 'https://panx.asia/'
+    print('Start parsing ptt hot....')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for data in soup.select('div.container div.row div.desc_wrap h2 a'):
+        title = data.text
+        link = data['href']
+        content += '{}\n{}\n\n'.format(title, link)
+    return content
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+	
+    if event.message.text == "eyny":
+        content = eyny_movie()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "news":
+        content = apple_news()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "av":
+        content = av()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "beauty":
+        content = ptt_beauty()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "hot":
+        content = ptt_hot()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "gossiping":
+        content = ptt_gossiping()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "atmovie":
+        content = movie()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "true_movie":
+        target_url = 'https://www.youtube.com/user/truemovie1/videos'
+        rs = requests.session()
+        res = rs.get(target_url, verify=False)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        seqs = ['https://www.youtube.com{}'.format(data.find('a')['href']) for data in soup.select('.yt-lockup-title')]
+        line_bot_api.reply_message(
+            event.reply_token, [
+                TextSendMessage(text=seqs[random.randint(0, len(seqs) - 1)]),
+                TextSendMessage(text=seqs[random.randint(0, len(seqs) - 1)])
+            ])
+        return 0
+
+    
+    if event.message.text == "technews":
+        content = technews()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    if event.message.text == "panx":
+        content = panx()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+    
+    else:
+        content = '輸入"eyny":eyny電影版連結網址。\n輸入"news":apple news即時新聞。\n輸入"beauty":PTT 表特版 近期大於 10 推的文章。\n輸入"hot":近期熱門廢文。\n輸入"gossiping":即時廢文。\n輸入"atmovie":近期上映電影。\n輸入"true_movie":觸電網-youtube。\n輸入"technews":科技新報。\n輸入"panx":PanX泛科技。\n'
+        line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=content))
+        return 0
+
+
+if __name__ == "__main__":
+    app.run()
